80df7c04994c22faa7ac6806a33a6c4a69afc843
banboobee <qzh00033@nifty.ne.jp>
2024-11-09 11:21:54 +0900

    More logs for device.

--- test/broadlink.test.js
+++ test/broadlink.test.js
@@ -15,8 +15,9 @@ describe('Broadlink device', () => {
     console.log('\tFound', devices.length, 'broadlink devices:');
     devices.forEach(async (device) => {
       const host = broadlink.devices[device].host;
+      const {name, lock} = await broadlink.devices[device].getDeviceName(0);
       const v = await broadlink.devices[device].getFWversion(0);
-      console.log(`\t${host.macAddress} ${host.address} ${broadlink.devices[device].model}(v${v})`);
+      console.log(`\t${host.macAddress} ${host.address} ${broadlink.devices[device].model} (v${v})\t${name} (${lock ? 'locked' : 'unlocked'})`);
     });
     await delayForDuration(1);
 
